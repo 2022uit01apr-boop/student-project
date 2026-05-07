f657c9fc3197d58c924a1b2cb108b667d343e789
Aparna <aparnavenkatesh05@gmail.com>
2026-05-07 12:31:25 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+import mysql.connector
+
+app = Flask(__name__)
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="studentdb"
+)
+
+cursor = db.cursor()
+
+@app.route('/add', methods=['POST'])
+def add():
+    name = request.form['name']
+    cursor.execute("INSERT INTO students (name) VALUES (%s)", (name,))
+    db.commit()
+    return "Data Stored Successfully"
+
+app.run(debug=True)  
